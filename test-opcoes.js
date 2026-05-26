a9d03ac3e7b4b2e40141eaf7a522a6c2e3d9558b
agomes <agomes@foccusgestao.com.br>
2026-05-25 22:52:32 -0300

    chore(repo): remover arquivo de teste acidental

--- test-opcoes.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-const { fetchOpcoesNetOptionQuotes } = require('./dist/core/invest/opcoesNetQuotes.js');
-fetchOpcoesNetOptionQuotes(['WEGEF476']).then(res => console.log(res));
