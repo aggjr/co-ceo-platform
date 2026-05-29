d5d0f73197222a7fcb319094d8759c3594d64bf4
Augusto G. Gomes <augustoggomes@yahoo.com.br>
2026-05-29 18:01:42 -0300

    fix: remover referencias a paginas deletadas pelo branch guto

--- frontend/src/legacy/legacyRoutes.ts
+++ frontend/src/legacy/legacyRoutes.ts
@@ -23,9 +23,6 @@ import { InvestResultadoPage } from '../pages/InvestResultadoPage.js';
 import { InvestClosedTradesPage } from '../pages/InvestClosedTradesPage.js';
 import { InvestStockGainPivotPage } from '../pages/InvestStockGainPivotPage.js';
 import { InvestHistoricoOperacoesPage } from '../pages/InvestHistoricoOperacoesPage.js';
-import { InvestExtratosPage } from '../pages/InvestExtratosPage.js';
-import { InvestImportacaoPage } from '../pages/InvestImportacaoPage.js';
-import { InvestImportacaoMesPage } from '../pages/InvestImportacaoMesPage.js';
 import { InvestConciliacaoPage } from '../pages/InvestConciliacaoPage.js';
 import { QualityRegressionPage } from '../pages/QualityRegressionPage.js';
 
@@ -57,9 +54,6 @@ export const LEGACY_ROUTES: LegacyRouteDef[] = [
   { path: '/invest/resultado', loader: InvestResultadoPage },
   { path: '/invest/ganhos-por-acao', loader: InvestStockGainPivotPage },
   { path: '/invest/historico-operacoes', loader: InvestHistoricoOperacoesPage },
-  { path: '/invest/extratos', loader: InvestExtratosPage },
-  { path: '/invest/importacao', loader: InvestImportacaoPage },
-  { path: '/invest/importacao-mes', loader: InvestImportacaoMesPage },
   { path: '/invest/conciliacao', loader: InvestConciliacaoPage },
   { path: '/invest/transacoes-finalizadas', loader: InvestClosedTradesPage },
 ];
